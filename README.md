5f67bc506f63747a9c2c1b8284bc0a5b01c9ad08
Hao Feng <fenghaolw@gmail.com>
2017-08-06 17:33:17 -0700

    Adjust field names and setup ESlint (#9)
    * Add an method for general InsertEntry and change other existed methods to use it.
    
    * Update main.js
    
    * Update main.js
    
    * Update main.js
    
    * Hide password and update field names.
    
    * Change field names to match the database.
    
    * Setup ESLint configuration and change code to be ESLint compatible.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # happy-rater
 ML Training Data Labeling Tool
 
+## Setup ESLint
+Use Google's Javascript guide rules as a baseline.
+
+    $ npm install --save-dev eslint eslint-config-google
+
+We also enable ES6 syntax by default. Check `.eslintrc` for more information.
+
 2017
--- data_store/main.js
+++ data_store/main.js
@@ -9,64 +9,94 @@ const con = mysql.createConnection({
   user: 'ratermaster',
   // TODO(gillwang): find a way to solve the password leak issue of mysql.
   password: '',
-  database: 'happyrater'
+  database: 'happyrater',
 });
 
 con.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    throw err;
+  }
   console.log('Connected!');
 });
 
-const querySql = function(sql) {
+function querySql(sql, msg) {
   con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log('Inserted one entry.');
+    if (err) {
+      throw err;
+    }
+    if (msg) {
+      console.log(msg);
+    }
+    console.log(result);
   });
-};
+}
 
 // TODO(fenghaolw): All of these should support batch updates.
 // entry is a ES6 Map
-const InsertEntry = function(table_name, entry) {
+function insertEntry(tableName, entry) {
   // Print the entry for debugging
   console.log(entry);
   const fields = Array.from(entry.keys()).join(',');
   const values = Array.from(entry.values()).join(',');
-  querySql(`INSERT INTO ${table_name} (${fields}) VALUES (${values});`);
-};
+  querySql(`INSERT INTO ${tableName} (${fields}) VALUES (${values});`,
+    'Inserted one entry.');
+}
 
-const AddTask = function(type) {
+function addRater(id, account, nickname) {
   const entry = new Map();
-  entry.set('type', type);
-  InsertEntry('task', entry);
-};
+  entry.set('rater_id', id);
+  entry.set('rater_account', `'${account}'`);
+  entry.set('rater_nickname', `'${nickname}'`);
+  insertEntry('rater', entry);
+}
 
-const AddRater = function(rater_id, first_name, last_name) {
+function addRequester(id, account) {
   const entry = new Map();
-  entry.set('id', rater_id);
-  entry.set('first_name', `'${first_name}'`);
-  entry.set('last_name', `'${last_name}'`);
-  InsertEntry('rater', entry);
-};
+  entry.set('requester_id', id);
+  entry.set('requester_account', `'${account}'`);
+  insertEntry('requester', entry);
+}
 
-const AddQuestion = function(question_id, description, image_url, task_id) {
+function addTask(id, requesterId, name, type, instruction, question,
+  answer, timestamp) {
   const entry = new Map();
-  entry.set('id', question_id);
-  entry.set('task_id', task_id);
-  entry.set('description', `'${description}'`);
-  entry.set('image_url', `'${image_url}'`);
-  InsertEntry('question', entry);
-};
+  entry.set('task_id', id);
+  entry.set('task_requester_id', requesterId);
+  entry.set('task_name', name);
+  // TODO(fenghaolw): type is a enum. Check if any special handling is required.
+  entry.set('task_type', type);
+  entry.set('task_instruction', instruction);
+  entry.set('task_question_string', question);
+  entry.set('task_answer_options', answer);
+  entry.set('task_creation_timestamp', timestamp);
+  insertEntry('task', entry);
+}
 
-const AddRecord = function(record_id, rater_id, question_id, response) {
+function addQuestion(id, taskId, imageUrl) {
   const entry = new Map();
-  entry.set('id', record_id);
-  entry.set('rater_id', rater_id);
-  entry.set('question_id', question_id);
-  entry.set('response', `'${response}'`);
-  InsertEntry('record', entry);
-};
+  entry.set('question_id', id);
+  entry.set('task_id', taskId);
+  entry.set('image_url', `'${imageUrl}'`);
+  insertEntry('question', entry);
+}
 
-eventEmitter.on('AddTask', AddTask);
-eventEmitter.on('AddRater', AddRater);
-eventEmitter.on('AddQuestion', AddQuestion);
-eventEmitter.on('AddRecord', AddRecord);
+function addAnswer(id, questionId, raterId, answer) {
+  const entry = new Map();
+  entry.set('answer_id', id);
+  entry.set('question_id', questionId);
+  entry.set('answer_rater_id', raterId);
+  entry.set('answer_string', `'${answer}'`);
+  insertEntry('answer', entry);
+}
+
+eventEmitter.on('AddRater', addRater);
+eventEmitter.on('AddRequester', addRequester);
+eventEmitter.on('AddTask', addTask);
+eventEmitter.on('AddQuestion', addQuestion);
+eventEmitter.on('AddAnswer', addAnswer);
+
+exports.addRater = addRater;
+exports.addRequester = addRequester;
+exports.addTask = addTask;
+exports.addQuestion = addQuestion;
+exports.addAnswer = addAnswer;
