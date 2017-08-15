d77bc4e626a56d47b39ad016cf06f67e156b5438
Yao <xyrhythm@users.noreply.github.com>
2017-08-15 10:08:14 -0700

    Change timestamps to be native unix timestamps (ints). (#24)
    * Change timestamps to be native unix timestamps (ints).
    
    * Change Tabs to Spaces.

--- data_store/schema.sql
+++ data_store/schema.sql
@@ -1,21 +1,22 @@
 CREATE TABLE IF NOT EXISTS rater (
-	rater_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  rater_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   # email.
   account VARCHAR(128) NOT NULL UNIQUE,
   password VARCHAR(128) NOT NULL,
   username VARCHAR(32) NOT NULL,
-  created_timestamp TIMESTAMP NOT NULL,
-  modified_timestamp TIMESTAMP NOT NULL
+  # unix timestamp in seconds.
+  created_timestamp INT UNSIGNED NOT NULL,
+  modified_timestamp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS requester (
-	requester_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  requester_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   # email
   account VARCHAR(128) NOT NULL UNIQUE,
   password VARCHAR(128) NOT NULL,
   username VARCHAR(32) NOT NULL,
-  created_timestamp TIMESTAMP NOT NULL,
-  modified_timestamp TIMESTAMP NOT NULL
+  created_timestamp INT UNSIGNED NOT NULL,
+  modified_timestamp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS admin (
@@ -23,21 +24,21 @@ CREATE TABLE IF NOT EXISTS admin (
   account VARCHAR(128) NOT NULL PRIMARY KEY,
   password VARCHAR(128) NOT NULL,
   username VARCHAR(32) NOT NULL,
-  created_timestamp TIMESTAMP NOT NULL,
-  modified_timestamp TIMESTAMP NOT NULL
+  created_timestamp INT UNSIGNED NOT NULL,
+  modified_timestamp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS task (
-	task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   # foreign key?
   requester_id INT NOT NULL,
-	name VARCHAR(128) NOT NULL,
+  name VARCHAR(128) NOT NULL,
   type ENUM('detection', 'classification', 'segmentation') NOT NULL,
   instruction VARCHAR(256) NOT NULL,
   question_string VARCHAR(128) NOT NULL,
   answer_options_json TEXT,
-  created_timestamp TIMESTAMP NOT NULL,
-  modified_timestamp TIMESTAMP NOT NULL
+  created_timestamp INT UNSIGNED NOT NULL,
+  modified_timestamp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ratertask (
@@ -56,16 +57,16 @@ CREATE TABLE IF NOT EXISTS requestertask (
 );
 
 CREATE TABLE IF NOT EXISTS question (
-	question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   # foreign key?
   task_id INT NOT NULL,
-	image_url VARCHAR(256)
+  image_url VARCHAR(256)
 );
 
 CREATE TABLE IF NOT EXISTS answer (
   task_id INT NOT NULL,
   question_id INT NOT NULL,
   rater_id INT NOT NULL,
-	answer_json TEXT,
-	PRIMARY KEY(task_id, question_id, rater_id)
+  answer_json TEXT,
+  PRIMARY KEY(task_id, question_id, rater_id)
 );
